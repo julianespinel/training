a76c63587ff600c00d3148b317460796dc47f9f3
Julian Espinel <julianespinel@gmail.com>
2015-12-23 18:23:25 -0500

    Add HackerRank first problem.

---
+++ hackerrank/is-fibo.go
@@ -0,0 +1,49 @@
+package main
+
+import "os"
+import "fmt"
+import "log"
+import "math"
+import "bufio"
+import "strconv"
+
+func getTestCases() []int {
+	testCases := []int{}
+	line := bufio.NewScanner(os.Stdin)
+	numberOfCases, err := strconv.Atoi(line.Text())
+	if numberOfCases >= 0 {
+		for i := 0; i < numberOfCases && line.Scan(); i++ {
+			number, _ := strconv.Atoi(line.Text())
+			testCases = append(testCases, number)
+		}
+	} else {
+		log.Println("else", err)
+	}
+	return testCases
+}
+
+func isPerfectSquare(number int) bool {
+	floatNumber := math.Sqrt(float64(number))
+	intNumber := float64(math.Trunc(floatNumber))
+	isPerfectSquare := (intNumber == floatNumber)
+	return isPerfectSquare
+}
+
+func isFibonacciNumber(number int) bool {
+	fiveByXSquare := 5 * number * number;
+	isFibonacciNumber := isPerfectSquare(fiveByXSquare + 4) || isPerfectSquare(fiveByXSquare - 4);
+	return isFibonacciNumber
+}
+
+func main() {
+	// testCases := getTestCases();
+	testCases := []int{5,7,8}
+	for _, element := range testCases {
+		log.Println("element", element)
+		if (isFibonacciNumber(element)) {
+			fmt.Println("IsFibo")
+		} else {
+			fmt.Println("IsNotFibo")
+		}
+	}
+}
