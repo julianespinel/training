dbcc67754f854ad448474cc27907cd8cbf7f8749
Julian Espinel <julianespinel@gmail.com>
2015-12-27 20:48:54 -0500

    Add test case 6 and test it.

--- hackerrank/is-fibo.go
+++ hackerrank/is-fibo.go
@@ -1,49 +1,52 @@
 package main
 
-import "os"
 import "fmt"
-import "log"
 import "math"
-import "bufio"
-import "strconv"
+import "math/big"
 
 func getTestCases() []int {
 	testCases := []int{}
-	line := bufio.NewScanner(os.Stdin)
-	numberOfCases, err := strconv.Atoi(line.Text())
-	if numberOfCases >= 0 {
-		for i := 0; i < numberOfCases && line.Scan(); i++ {
-			number, _ := strconv.Atoi(line.Text())
-			testCases = append(testCases, number)
+	var numberOfLines int
+	fmt.Scanf("%d", &numberOfLines)
+
+	if numberOfLines > 0 {
+		for i := 0; i < numberOfLines; i++ {
+			var testCase int
+			fmt.Scanf("%d", &testCase)
+			testCases = append(testCases, testCase)
 		}
-	} else {
-		log.Println("else", err)
-	}
+	}	
 	return testCases
 }
 
 func isPerfectSquare(number int) bool {
 	floatNumber := math.Sqrt(float64(number))
-	intNumber := float64(math.Trunc(floatNumber))
-	isPerfectSquare := (intNumber == floatNumber)
+	// intNumber := float64(math.Trunc(floatNumber))
+	intNumber := big.NewFloat(floatNumber)
+	// isPerfectSquare := (intNumber == floatNumber)
+	isPerfectSquare := intNumber.IsInt()
+	fmt.Println(number)
+	fmt.Println(floatNumber)
+	fmt.Println(intNumber)
+	fmt.Println(isPerfectSquare)
 	return isPerfectSquare
 }
 
 func isFibonacciNumber(number int) bool {
-	fiveByXSquare := 5 * number * number;
-	isFibonacciNumber := isPerfectSquare(fiveByXSquare + 4) || isPerfectSquare(fiveByXSquare - 4);
+	fiveByXSquare := 5 * number * number
+	isFibonacciNumber := isPerfectSquare(fiveByXSquare+4) || isPerfectSquare(fiveByXSquare-4)
 	return isFibonacciNumber
 }
 
 func main() {
 
-	// https://www.hackerrank.com/challenges/is-fibo
-
-	// testCases := getTestCases();
-	testCases := []int{5,7,8}
+	// https://www.hackerrank.com/challenges/is-fibo	
+	// testCases := getTestCases()
+	testCases := []int{7778742049}
+	// testCases := []int{5,7,8}
+	
 	for _, element := range testCases {
-		log.Println("element", element)
-		if (isFibonacciNumber(element)) {
+		if isFibonacciNumber(element) {
 			fmt.Println("IsFibo")
 		} else {
 			fmt.Println("IsNotFibo")
