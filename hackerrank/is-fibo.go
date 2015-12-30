7d38182586328729d3531678e5374dab8ce25f3d
Julian Espinel <julianespinel@gmail.com>
2015-12-29 20:59:54 -0500

    Complete is-fibo problem.

--- hackerrank/is-fibo.go
+++ hackerrank/is-fibo.go
@@ -2,7 +2,6 @@ package main
 
 import "fmt"
 import "math"
-import "math/big"
 
 func getTestCases() []float64 {
 	testCases := []float64{}
@@ -12,37 +11,41 @@ func getTestCases() []float64 {
 	if numberOfLines > 0 {
 		for i := 0; i < numberOfLines; i++ {
 			var testCase float64
-			fmt.Scanf("%d", &testCase)
+			fmt.Scanf("%f", &testCase)
 			testCases = append(testCases, testCase)
 		}
 	}
+
+	// fmt.Println(numberOfLines)
+	// fmt.Println(testCases)
+
 	return testCases
 }
 
 func isPerfectSquare(number float64) bool {
 	floatNumber := math.Sqrt(float64(number))
-	// intNumber := float64(math.Trunc(floatNumber))
-	intNumber := big.NewFloat(floatNumber)
-	// isPerfectSquare := (intNumber == floatNumber)
-	isPerfectSquare := intNumber.IsInt()
-	fmt.Println(number)
-	fmt.Println(floatNumber)
-	fmt.Println(intNumber)
-	fmt.Println(isPerfectSquare)
+	// intNumber := big.NewFloat(floatNumber) // It seems like this line only works in go 1.5
+	// isPerfectSquare := intNumber.IsInt()
+	intNumber := float64(math.Trunc(floatNumber))
+	isPerfectSquare := (intNumber == floatNumber)
+	// fmt.Println(number)
+	// fmt.Println(floatNumber)
+	// fmt.Println(intNumber)
+	// fmt.Println(isPerfectSquare)
 	return isPerfectSquare
 }
 
 func isFibonacciNumber(number float64) bool {
 	fiveByXSquare := float64(5 * number * number)
-	isFibonacciNumber := isPerfectSquare(fiveByXSquare+4) || isPerfectSquare(fiveByXSquare-4)
+	isFibonacciNumber := (isPerfectSquare(fiveByXSquare+4) || isPerfectSquare(fiveByXSquare-4))
 	return isFibonacciNumber
 }
 
 func main() {
 
 	// https://www.hackerrank.com/challenges/is-fibo	
-	// testCases := getTestCases()
-	testCases := []float64{7778742049}
+	testCases := getTestCases()
+	// testCases := []float64{7778742049}
 	// testCases := []float64{5,7,8}
 	for _, element := range testCases {
 		if isFibonacciNumber(element) {
