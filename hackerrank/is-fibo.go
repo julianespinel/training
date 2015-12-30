97c3049ac11a4defd9b547cc5c135fc7a2a401bd
Julian Espinel <julianespinel@gmail.com>
2015-12-29 20:46:58 -0500

    Read test cases as float64 insted of int.

--- hackerrank/is-fibo.go
+++ hackerrank/is-fibo.go
@@ -4,22 +4,22 @@ import "fmt"
 import "math"
 import "math/big"
 
-func getTestCases() []int {
-	testCases := []int{}
+func getTestCases() []float64 {
+	testCases := []float64{}
 	var numberOfLines int
 	fmt.Scanf("%d", &numberOfLines)
 
 	if numberOfLines > 0 {
 		for i := 0; i < numberOfLines; i++ {
-			var testCase int
+			var testCase float64
 			fmt.Scanf("%d", &testCase)
 			testCases = append(testCases, testCase)
 		}
-	}	
+	}
 	return testCases
 }
 
-func isPerfectSquare(number int) bool {
+func isPerfectSquare(number float64) bool {
 	floatNumber := math.Sqrt(float64(number))
 	// intNumber := float64(math.Trunc(floatNumber))
 	intNumber := big.NewFloat(floatNumber)
@@ -32,8 +32,8 @@ func isPerfectSquare(number int) bool {
 	return isPerfectSquare
 }
 
-func isFibonacciNumber(number int) bool {
-	fiveByXSquare := 5 * number * number
+func isFibonacciNumber(number float64) bool {
+	fiveByXSquare := float64(5 * number * number)
 	isFibonacciNumber := isPerfectSquare(fiveByXSquare+4) || isPerfectSquare(fiveByXSquare-4)
 	return isFibonacciNumber
 }
@@ -42,9 +42,8 @@ func main() {
 
 	// https://www.hackerrank.com/challenges/is-fibo	
 	// testCases := getTestCases()
-	testCases := []int{7778742049}
-	// testCases := []int{5,7,8}
-	
+	testCases := []float64{7778742049}
+	// testCases := []float64{5,7,8}
 	for _, element := range testCases {
 		if isFibonacciNumber(element) {
 			fmt.Println("IsFibo")
