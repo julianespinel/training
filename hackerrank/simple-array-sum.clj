dfd25ada66fc5b0d84428c195a03af01e9b574eb
Julian Espinel <julianespinel@gmail.com>
2016-04-21 21:19:47 -0500

    First submission of the simple array sum.

--- hackerrank/simple-array-sum.clj
+++ hackerrank/simple-array-sum.clj
@@ -1,17 +1,9 @@
 ; https://www.hackerrank.com/challenges/simple-array-sum
 (use '[clojure.string :only (split triml)])
 
-(
- let [
-      n_t (read-line) 
+(let [n_t (read-line) 
       n (Integer/parseInt n_t) 
-      ]
-
- (
-  let [
-       arr_temp (read-line) 
-       arr_t (split arr_temp #"\s+") 
-       arr (map #(Integer/parseInt %) arr_t)
-       ]
-  )
-)
+      arr_temp (read-line)
+      arr_t (split arr_temp #"\s+") 
+      arr (map #(Integer/parseInt %) arr_t)]
+  (println(reduce + arr)))
