f34cf9ef1dff6d824fca863da07112ed3f9dfd42
Julian Espinel <julianespinel@gmail.com>
2016-04-25 14:01:23 -0500

    Resolve a very big sum.

---
+++ hackerrank/a-very-big-sum.clj
@@ -0,0 +1,9 @@
+; https://www.hackerrank.com/challenges/a-very-big-sum
+(use '[clojure.string :only (split triml)])
+
+(let [n_t (read-line) 
+      n (Integer/parseInt n_t) 
+      arr_temp (read-line) 
+      arr_t (split arr_temp #"\s+") 
+      arr (map #(Integer/parseInt %) arr_t)]
+  (println(reduce + arr)))
