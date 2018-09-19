04cb48c6a43efc7de9529400fbd842f32ea1cba4
Julian Espinel <julianespinel@gmail.com>
2018-09-18 21:46:00 -0500

    WIP: first version of the solution, it is not correct yet.

---
+++ hackerrank/PlusMinus.hs
@@ -0,0 +1,26 @@
+-- https://www.hackerrank.com/challenges/plus-minus/problem
+
+readInputList :: IO([Int])
+readInputList = do
+  listSize <- readLn :: IO Int
+  line <- getLine
+  let inputList = map (read :: String -> Int) $ words line
+  return inputList
+
+getNumbers :: [Int] -> [Int]
+getNumbers inputList =
+  let positives = filter (<0) inputList
+      zeros = filter (==0) inputList
+      negatives = filter (>0) inputList
+   in map length $ positives:zeros:negatives:[]
+  -- let functions = [<0, ==0, >0]
+  -- zipWith functions inputList
+
+ratio :: Int -> Int -> Double
+ratio listSize number = fromIntegral(number) / fromIntegral(listSize)
+
+main = do
+  inputList <- readInputList
+  let numbers = getNumbers(inputList) -- numbers = positives:zeros:negatives
+  let results = map (ratio (length inputList)) numbers
+  mapM_ print results
