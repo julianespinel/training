1c33ad3e59f1d39bd2d4749e70dca9626c0d6ea9
Julian Espinel <julianespinel@gmail.com>
2018-09-25 22:17:28 -0500

    Improve solution to O(n) complexity.

--- hackerrank/ClimbingTheLaderboard.hs
+++ hackerrank/ClimbingTheLaderboard.hs
@@ -9,25 +9,21 @@ readScores size = do
   let leaderboard = map (read :: String -> Int) $ words line
   assert (size == length leaderboard) (return leaderboard)
 
-rankScore :: [Int] -> [Int] -> Int -> ([Int], Int)
-rankScore initial [] score = (initial ++ [score], length initial)
-rankScore initial (x:xs) score
-  | score > x = (initial ++ score:x:xs, length initial)
-  | score == x = (initial ++ x:xs, length initial)
-  | score < x = rankScore (initial ++ [x]) xs score
-
-getRanks :: [Int] -> [Int] -> [Int] -> [Int]
-getRanks leaderboard [] ranks  = ranks
-getRanks leaderboard (score:scores) ranks =
-  let (newLeaderboard, rank) = rankScore [] leaderboard score
-   in getRanks newLeaderboard scores (ranks ++ [rank])
+getRanks :: [Int] -> [Int] -> Int -> [Int]
+getRanks [] scores index = replicate (length scores) index
+getRanks leaderboard [] index = []
+getRanks (x:xs) (score:scores) index
+  | score < x = getRanks xs (score:scores) (index + 1)
+  | score == x = index:getRanks (x:xs) scores index
+  | score > x = index:getRanks (x:xs) scores index
 
 main = do
   leaderboardSize <- readLn :: IO Int
   leaderboard <- readScores leaderboardSize
+  let leaderboardSet = (Set.toDescList . Set.fromDescList) leaderboard
   aliceGamesSize <- readLn :: IO Int
   aliceGames <- readScores aliceGamesSize
-  let leaderboardNoDuplicates = (Set.toDescList . Set.fromDescList) leaderboard
-  let aliceRanks = getRanks leaderboardNoDuplicates aliceGames []
-  let correctedIndicesRanks = map (+1) aliceRanks
+  let aliceGamesDesc = reverse aliceGames
+  let aliceRanks = getRanks leaderboardSet aliceGamesDesc 0
+  let correctedIndicesRanks = map (+1) $ reverse aliceRanks
   mapM_ print correctedIndicesRanks
