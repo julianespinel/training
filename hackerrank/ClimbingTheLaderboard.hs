0938c9ac0a2c8915cc78c368dec7287f23b38458
Julian Espinel <julianespinel@gmail.com>
2018-09-26 21:43:56 -0500

    Document each function of the program.

--- hackerrank/ClimbingTheLaderboard.hs
+++ hackerrank/ClimbingTheLaderboard.hs
@@ -6,12 +6,31 @@ module Main where
 import Control.Exception.Base(assert)
 import qualified Data.Set as Set
 
+-- | The 'readScores' function takes a value indicating the amount
+-- of words to read from stdin.
+--
+-- The function turns the words read from stdin into to a list of 'Int's
+-- and returns it inside the IO monad.
 readScores :: Int -> IO([Int])
 readScores size = do
   line <- getLine
   let leaderboard = map (read :: String -> Int) $ words line
   assert (size == length leaderboard) (return leaderboard)
 
+-- | The 'getRanks' fucntion takes three parameters:
+--
+-- 1. leaderboard: a list of 'Int's in descending order, representing
+--   a leaderboard.
+--
+-- 2. scores: a list of 'Int's in descending order, representing the scores
+--   of a single player.
+--
+-- 3. index: an 'Int' to represent the current position we are evaluating in
+--   in the leaderboard.
+--
+-- This function returns a list of 'Int's. Each element in the result
+-- represents the position of the user in the leaderboard per each score
+-- given in the second parameter 'scores'.
 getRanks :: [Int] -> [Int] -> Int -> [Int]
 getRanks [] scores index = replicate (length scores) index
 getRanks leaderboard [] index = []
@@ -20,6 +39,8 @@ getRanks (x:xs) (score:scores) index
   | score == x = index:getRanks (x:xs) scores index
   | score > x = index:getRanks (x:xs) scores index
 
+-- | The 'main' function is the entry point of the program
+main :: IO()
 main = do
   leaderboardSize <- readLn :: IO Int
   leaderboard <- readScores leaderboardSize
