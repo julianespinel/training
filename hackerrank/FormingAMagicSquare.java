08a0da8ac838c3835822e2d9e10ff2e29e26012d
Julian Espinel <julianespinel@gmail.com>
2018-09-28 21:53:46 -0500

    Solve problem in O(n) complexity.

--- hackerrank/FormingAMagicSquare.java
+++ hackerrank/FormingAMagicSquare.java
@@ -10,18 +10,20 @@ public class FormingAMagicSquare {
     private static final int ROWS = 3;
     private static final String SPACE = " ";
 
-    private static final int[][] MAGIC_SQUARES = new int[][]{
-            {8,1,6,3,5,7,4,9,2},
-            {6,1,8,7,5,3,2,9,4},
-            {4,9,2,3,5,7,8,1,6},
-            {2,9,4,7,5,3,6,1,8},
-            {8,3,4,1,5,9,6,7,2},
-            {4,3,8,9,5,1,2,7,6},
-            {6,7,2,1,5,9,8,3,4},
-            {2,7,6,9,5,1,4,3,8}
+    private static final int[] MAGIC_SQUARES_LINEAR = new int[]{
+            8,1,6,3,5,7,4,9,2,
+            6,1,8,7,5,3,2,9,4,
+            4,9,2,3,5,7,8,1,6,
+            2,9,4,7,5,3,6,1,8,
+            8,3,4,1,5,9,6,7,2,
+            4,3,8,9,5,1,2,7,6,
+            6,7,2,1,5,9,8,3,4,
+            2,7,6,9,5,1,4,3,8
     };
 
-    private static final int MAGIC_SQUARE_SUM = 45;
+    private static final int ELEMETS_IN_MAGIC_SQUARE = 9;
+    private static final int NUMBER_OF_MAGIC_SQUARES =
+            MAGIC_SQUARES_LINEAR.length / ELEMETS_IN_MAGIC_SQUARE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -51,27 +53,26 @@ public class FormingAMagicSquare {
         return numbers;
     }
 
-    /*
-     TODO: is possible to solve this problem in O(n) by making a large 1D
-     array of all possible 3x3 magic squares.
-    */
     private static int convertToMagicSquareAtMinCost(int[] flatMatrix) {
-        List<Integer> costs = new ArrayList<>(MAGIC_SQUARES.length);
-        for (int[] magicSquare : MAGIC_SQUARES) {
-            int cost = getMinCost(magicSquare, flatMatrix);
-            costs.add(cost);
-        }
+        List<Integer> costs = getMinCostLinear(MAGIC_SQUARES_LINEAR, flatMatrix);
         return Collections.min(costs);
     }
 
-    private static int getMinCost(int[] magicSquare, int[] flatMatrix) {
+    private static List<Integer> getMinCostLinear(int[] magicSquares, int[] flatMatrix) {
+        List<Integer> costs = new ArrayList<>();
         int cost = 0;
-        assert magicSquare.length == flatMatrix.length;
-        for (int i = 0; i < magicSquare.length; i++) {
-            int elementOne = magicSquare[i];
-            int elementTwo = flatMatrix[i];
+        assert magicSquares.length == flatMatrix.length * NUMBER_OF_MAGIC_SQUARES;
+        for (int i = 0; i < magicSquares.length; i++) {
+            int flatMatrixIndex = i % ELEMETS_IN_MAGIC_SQUARE;
+            if (i > 0 && flatMatrixIndex == 0) {
+                costs.add(cost);
+                cost = 0;
+            }
+            int elementOne = magicSquares[i];
+            int elementTwo = flatMatrix[flatMatrixIndex];
             cost += Math.abs(elementOne - elementTwo);
         }
-        return cost;
+        costs.add(cost);
+        return costs;
     }
 }
