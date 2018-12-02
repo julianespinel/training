5fa7ed0ef33e6a153bcebb936732db3f41bbe7ec
Julian Espinel <julianespinel@gmail.com>
2018-12-01 22:09:09 -0500

    Improve solution to past test cases with no timeout.
    The improvement involved caching the maximum value computed only,
    instead of storing an array of all previously computed values.

--- hackerrank/MaxArraySum.java
+++ hackerrank/MaxArraySum.java
@@ -1,11 +1,13 @@
-import java.util.*;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
+// https://www.hackerrank.com/challenges/max-array-sum/problem
 public class MaxArraySum {
 
   private static final String SPACE = " ";
+  private static final long SENTINEL = -1;
 
   private static int[] readNumbers() throws IOException {
     try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
@@ -15,6 +17,16 @@ public class MaxArraySum {
     }
   }
 
+  /**
+   * Because if the restriction of the problem (find the subset of non-adjacent
+   * elements with the maximum sum), this method was designed to skip the next
+   * element relative to the currentIndex and return the indices of the next
+   * two positive elements.
+   *
+   * @param currentIndex Index of the numbers array we are currently evaluating.
+   * @param numbers An array of numbers.
+   * @return a List with at most two elements.
+   */
   private static List<Integer> findNextIndices(int currentIndex, int[] numbers) {
     List<Integer> nextIndices = new ArrayList<>();
     for (int i = currentIndex + 2; i < numbers.length; i++) {
@@ -30,45 +42,47 @@ public class MaxArraySum {
     return nextIndices;
   }
 
-  private static void addToCache(Map<Integer, List<Long>> cache,
-      int key, long value) {
-    List<Long> cachedResults = cache.getOrDefault(key, new ArrayList<>());
-    if (!cachedResults.contains(value)) {
-      cachedResults.add(value);
-      cache.put(key, cachedResults);
+  /**
+   * Adds an element to a map only if the element is greater than the cached
+   * element corresponding to the given key.
+   *
+   * @param cache A map we are using to store previously computed values.
+   * @param key A possible key of the cache.
+   * @param value A value to be inserted in the cache.
+   */
+  private static void addToCache(Map<Integer, Long> cache, int key, long value) {
+    long cachedValue = cache.getOrDefault(key, SENTINEL);
+    if (value > cachedValue) {
+      cache.put(key, value);
     }
   }
 
-  private static long getMaxValue(Map<Integer, List<Long>> cache) {
+  private static long getMaxValue(Map<Integer, Long> cache) {
     long maxValue = 0;
-    for (List<Long> cachedValues : cache.values()) {
-      for (long value : cachedValues) {
-        maxValue = (value > maxValue) ? value : maxValue;
-      }
+    for (long value : cache.values()) {
+      maxValue = (value > maxValue) ? value : maxValue;
     }
     return maxValue;
   }
 
   private static long getMaxSum(int[] numbers) {
-    Map<Integer, List<Long>> cache = new HashMap<>();
+    Map<Integer, Long> cache = new HashMap<>();
     for (int i = 0; i < numbers.length; i++) {
       int currentNumber = numbers[i];
       if (currentNumber <= 0) {
         continue;
       }
       List<Integer> nextIndices = findNextIndices(i, numbers);
-      List<Long> cachedResults = cache.getOrDefault(i, null);
-      if (cachedResults == null) {
+      long cachedResult = cache.getOrDefault(i, SENTINEL);
+      if (cachedResult == SENTINEL) {
         for (int nextIndex : nextIndices) {
           long value = currentNumber + numbers[nextIndex];
           addToCache(cache, nextIndex, value);
         }
       } else {
         for (int nextIndex : nextIndices) {
-          for (long cachedResult : cachedResults) {
-            long value = cachedResult + numbers[nextIndex];
-            addToCache(cache, nextIndex, value);
-          }
+          long value = cachedResult + numbers[nextIndex];
+          addToCache(cache, nextIndex, value);
         }
       }
     }
