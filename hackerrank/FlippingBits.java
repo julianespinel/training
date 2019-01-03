7e63522042ab79068ef4d082f498995573a9d93f
Julian Espinel <julianespinel@gmail.com>
2019-01-02 23:17:24 -0500

    Comment methods that transform numbers to bits using the Java API and do it manually.

--- hackerrank/FlippingBits.java
+++ hackerrank/FlippingBits.java
@@ -13,9 +13,26 @@ public class FlippingBits {
   }
 
   private static String toBits(long number) {
-    String bits = Long.toBinaryString(number);
+    StringBuilder builder = new StringBuilder();
+    long remainder = number % 2;
+    long result = Math.floorDiv(number, 2);
+    builder.append(remainder);
+    while (result != 0) {
+      remainder = result % 2;
+      result = Math.floorDiv(result, 2);
+      builder.append(remainder);
+    }
+    String bits = builder.reverse().toString();
     return addLeadingZeros(bits);
   }
+
+//  /*
+//   * Method to go from a number to an array of 32 bits using the Java API.
+//   */
+//  private static String toBits(long number) {
+//    String bits = Long.toBinaryString(number);
+//    return addLeadingZeros(bits);
+//  }
 
   private static String flipBits(String bits) {
     String flippedBits = "";
@@ -27,8 +44,27 @@ public class FlippingBits {
   }
 
   private static long toUnsignedNumber(String bits) {
-    return Long.parseUnsignedLong(bits, 2);
+    long unsignedNumber = 0;
+    char[] chars = bits.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      if (chars[i] == '1') {
+        /*
+         * We use (chars.length - 1), because the conversion is zero-indexed.
+         * 32 bits From 31 to 0.
+         */
+        unsignedNumber += Math.pow(2, chars.length - 1 - i);
+      }
+    }
+    return unsignedNumber;
   }
+
+//  /*
+//   * Method to go from an array of bits to an unsigned number
+//   * using the Java API.
+//   */
+//  private static long toUnsignedNumber(String bits) {
+//    return Long.parseUnsignedLong(bits, 2);
+//  }
 
   public static void main(String[] args) throws IOException {
     try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
