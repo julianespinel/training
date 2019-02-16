fdbd66dd3b76f5bbbac32c9a342327a7b812cf6f
Julian Espinel <julianespinel@gmail.com>
2019-02-16 18:28:52 -0500

    Encapsulate code in methods to improve readability.

--- hackerrank/Candies.java
+++ hackerrank/Candies.java
@@ -10,6 +10,10 @@ public class Candies {
 
   private static final int SENTINEL = -1;
 
+  //------------------------------------------------------------
+  // IO functions
+  // -----------------------------------------------------------
+
   private static int readSize(BufferedReader reader) throws IOException {
     String line = reader.readLine();
     return Integer.parseInt(line);
@@ -24,6 +28,10 @@ public class Candies {
     return array;
   }
 
+  //------------------------------------------------------------
+  // Functions to calculate minimum candies to buy
+  // -----------------------------------------------------------
+
   private static int incrementAndAddCandies(int candies, Stack<Integer> stack) {
     candies++;
     stack.push(candies);
@@ -34,113 +42,82 @@ public class Candies {
     return (stack.isEmpty()) ? 0 : stack.peek();
   }
 
-  private static void calculateCandiesForLastElementInAscendingSequence(int index, int[] scores, Stack<Integer> stack, int candies) {
-    int currentScore = scores[index];
-    int previousScore = scores[index - 1];
-    if (previousScore < currentScore) {
-      incrementAndAddCandies(candies, stack);
-    } else if (previousScore == currentScore) {
-      candies = getLastElement(stack);
-      candies = (candies != 1) ? 1 : candies + 1;
-      stack.push(candies);
-    } else if (previousScore > currentScore) {
-      throw new IllegalStateException("Should never reach this line: calculateCandiesForAscendingSequence");
-    }
+  private static int getNextScore(int index, int[] scores) {
+    int nextIndex = index + 1;
+    return (nextIndex < scores.length) ? scores[nextIndex] : SENTINEL;
   }
 
   private static int calculateCandiesForAscendingSequence(int index, int[] scores, Stack<Integer> stack) {
     int candies = 0;
-    while (index < scores.length - 1) {
+    while (index < scores.length) {
       int currentScore = scores[index];
-      int nextScore = scores[index + 1];
-      if (currentScore < nextScore) {
-        candies = incrementAndAddCandies(candies, stack);
-      }
-      if (currentScore == nextScore) {
-        candies = getLastElement(stack);
-        candies = (candies != 1) ? 1 : candies + 1;
-        stack.push(candies);
-      }
-      if (currentScore > nextScore) {
-        // Sequence finishes
+      int nextScore = getNextScore(index, scores);
+      boolean isSequenceBroken = currentScore >= nextScore;
+      if (isSequenceBroken) {
         incrementAndAddCandies(candies, stack);
-        return index;
+        return index; // Sequence finishes
+      }
+      boolean isSequenceContinued = nextScore == SENTINEL || currentScore < nextScore;
+      if (isSequenceContinued) {
+        candies = incrementAndAddCandies(candies, stack);
       }
       index++;
     }
-    calculateCandiesForLastElementInAscendingSequence(index, scores, stack, candies);
     return index;
-  }
-
-  private static void calculateCandiesForLastElementInDescendingSequence(int index, int[] scores, Stack<Integer> stack, int candies) {
-    int currentScore = scores[index];
-    int previousScore = scores[index - 1];
-    if (previousScore < currentScore) {
-      throw new IllegalStateException("Should never reach this line: calculateCandiesForAscendingSequence");
-    } else if (previousScore == currentScore ) {
-      candies = getLastElement(stack);
-      candies = (candies != 1) ? 1 : candies + 1;
-      stack.push(candies);
-    } else if (previousScore > currentScore ) {
-      incrementAndAddCandies(candies, stack);
-    }
   }
 
   private static int calculateCandiesForDescendingSequence(int index, int[] scores, Stack<Integer> stack) {
     int candies = 0;
-    while (index < scores.length - 1) {
+    while (index < scores.length) {
       int currentScore = scores[index];
-      int nextScore = scores[index + 1];
-      if (currentScore < nextScore) {
-        // Sequence finishes
+      int nextScore = getNextScore(index, scores);
+      boolean isSequenceBroken = currentScore <= nextScore;
+      if (isSequenceBroken) {
         incrementAndAddCandies(candies, stack);
-        return index;
+        return index; // Sequence finishes
       }
-      if (currentScore == nextScore) {
-        candies = getLastElement(stack);
-        candies = (candies != 1) ? 1 : candies + 1;
-        stack.push(candies);
-      }
-      if (currentScore > nextScore) {
+      boolean isSequenceContinued = nextScore == SENTINEL || currentScore > nextScore;
+      if (isSequenceContinued) {
         candies = incrementAndAddCandies(candies, stack);
       }
       index++;
     }
-    // Check candies for the last student.
-    calculateCandiesForLastElementInDescendingSequence(index, scores, stack, candies);
     return index;
   }
 
-  private static int calculateCandiesForLastElement(int i, int[] scores, Stack<Integer> stack) {
-    if (scores.length == 1) {
-      stack.push(1);
-      return i;
-    }
+  private static void calculateCandiesForDuplicatedScores(Stack<Integer> stack) {
+    int candies = getLastElement(stack);
+    candies = (candies != 1) ? 1 : candies + 1;
+    stack.push(candies);
+  }
+
+  private static void calculateLastCandy(int i, int[] scores, Stack<Integer> stack) {
     int currentScore = scores[i];
     int previousScore = scores[i - 1];
     if (previousScore == currentScore) {
-      int candies = getLastElement(stack);
-      candies = (candies != 1) ? 1 : candies + 1;
-      stack.push(candies);
+      calculateCandiesForDuplicatedScores(stack);
+      return;
     }
-    return i;
+    incrementAndAddCandies(1, stack);
   }
 
   private static Collection<Integer> getMinimumCandiesToBuy(int[] scores) {
     Stack<Integer> stack = new Stack<>();
+    if (scores.length == 1) {
+      stack.push(1);
+      return stack;
+    }
+    // if scores length is greater than 1
     for (int i = 0; i < scores.length; i++) {
-      boolean isLastElement = i == scores.length - 1;
-      if (isLastElement) {
-        i = calculateCandiesForLastElement(i, scores, stack);
-      }
       int currentScore = scores[i];
-      int nextScore = scores[i + 1];
-      if (currentScore < nextScore) {
+      int nextScore = getNextScore(i, scores);
+      if (nextScore == SENTINEL) {
+        calculateLastCandy(i, scores, stack);
+        return stack;
+      } else if (currentScore < nextScore) {
         i = calculateCandiesForAscendingSequence(i, scores, stack);
-      } else if (currentScore == nextScore) {
-        int candies = getLastElement(stack);
-        candies = (candies != 1) ? 1 : candies + 1;
-        stack.push(candies);
+      } else if (currentScore == nextScore ) {
+        calculateCandiesForDuplicatedScores(stack);
       } else if (currentScore > nextScore) {
         i = calculateCandiesForDescendingSequence(i, scores, stack);
       }
@@ -155,6 +132,10 @@ public class Candies {
     }
     return accumulator;
   }
+
+  //------------------------------------------------------------
+  // Main function
+  // -----------------------------------------------------------
 
   public static void main(String[] args) throws IOException {
     try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
