16e0734c549dca691213ee8cd6103135c6170b40
Julian Espinel <julianespinel@gmail.com>
2019-12-02 22:23:25 +0200

    WIP: solution for binary string minimizing

---
+++ codeforces/BinaryStringMinimizer.hs
@@ -0,0 +1,45 @@
+-- http://codeforces.com/problemset/problem/1256/D
+
+data Case = Case { moves  :: Int
+                 , string :: String }
+
+
+getMoves :: String -> Int
+getMoves string = read $ last $ words string :: Int
+
+
+toCase :: (String, String) -> Case
+toCase (one, two) = Case { moves = getMoves one, string = two }
+
+
+toPairs :: [a] -> [(a,a)]
+toPairs []                  = []
+toPairs (first:second:tail) = (first, second) : toPairs tail
+toPairs _                   = error "list size is odd"
+
+
+isSolved :: String -> Bool
+isSolved []                  = True
+isSolved (first:second:tail) = first < second && isSolved tail
+isSolved _                   = True
+
+
+order :: String -> String
+order [] = []
+order (first:second:tail)
+  | first > second = second : first : tail
+  | otherwise = first : (order $ second : tail)
+order string = string
+
+
+solve :: Case -> String
+solve (Case moves string)
+  | moves == 0 = string
+  | isSolved string = string
+  | moves > 0 = solve Case { moves = moves - 1, string = order string }
+
+
+main :: IO ()
+main = interact $
+  unlines . map (show . solve . toCase) .
+  toPairs . tail . lines
