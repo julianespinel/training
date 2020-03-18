bf13d3f2c22737880f3cd84fc2deb20d343bd2d9
Julian Espinel <julianespinel@gmail.com>
2020-03-18 22:46:37 +0200

    Improve variable naming and code format

--- codeforces/BinaryStringMinimizer4.hs
+++ codeforces/BinaryStringMinimizer4.hs
@@ -4,8 +4,8 @@ import           Data.List
 import           Data.Maybe
 
 
-zero = '0'
-one = '1'
+zeroChar = '0'
+oneChar = '1'
 
 
 data Case = Case { moves  :: Int
@@ -29,33 +29,33 @@ toPairs (first:second:tail) = (first, second) : toPairs tail
 correctIndex :: Maybe Int -> Int
 correctIndex maybe
   | isNothing maybe = 0
-  | otherwise = fromJust maybe + 1
+  | otherwise       = fromJust maybe + 1
 
 
 calculateIndicesAfterMoves :: [Int] -> Int -> Maybe Int -> [Int]
 calculateIndicesAfterMoves [] moves lastZeroIndex = []
-calculateIndicesAfterMoves (x:xs) moves lastZeroIndex
-  | moves == 0 = x:xs
-  | otherwise = newIndex : calculateIndicesAfterMoves xs updatedMoves (Just newIndex)
+calculateIndicesAfterMoves initialZeros 0 _ = initialZeros
+calculateIndicesAfterMoves (initialZero:initialZeros) moves lastZeroIndex =
+  newIndex : calculateIndicesAfterMoves initialZeros updatedMoves (Just newIndex)
   where correctedIndex = correctIndex lastZeroIndex
-        desiredMoves = x - correctedIndex
-        allowedMoves = min desiredMoves moves
-        updatedMoves = moves - allowedMoves
-        newIndex = x - allowedMoves
+        desiredMoves   = initialZero - correctedIndex
+        allowedMoves   = min desiredMoves moves
+        updatedMoves   = moves - allowedMoves
+        newIndex       = initialZero - allowedMoves
 
 
 buildFinalList :: [Int] -> Int -> String -> String
 buildFinalList [] index string = string
-buildFinalList (x:xs) index (y:ys)
-  | x == index = zero : buildFinalList xs (index + 1) ys
-  | otherwise = y : buildFinalList (x:xs) (index + 1) ys
+buildFinalList (zero:zeros) index (one:ones)
+  | zero == index = zeroChar : buildFinalList zeros (index + 1) ones
+  | otherwise     = one : buildFinalList (zero:zeros) (index + 1) ones
 
 
 solve :: Case -> String
 solve (Case moves string) = do
-  let initialZeroIndices = elemIndices zero string
-  let finalZeroIndices = calculateIndicesAfterMoves initialZeroIndices moves Nothing
-  let ones = replicate (length string) one
+  let initialZeroIndices = elemIndices zeroChar string
+  let finalZeroIndices   = calculateIndicesAfterMoves initialZeroIndices moves Nothing
+  let ones               = replicate (length string) oneChar
   buildFinalList finalZeroIndices 0 ones
 
 
