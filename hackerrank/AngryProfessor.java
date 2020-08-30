9f4ff4a1df2f25650761d942a1c974751e69d9fc
Julian Espinel <julianespinel@gmail.com>
2020-08-30 14:21:09 +0300

    Re-organize code to make it easier to compare it against the Haskell implementation

--- hackerrank/AngryProfessor.java
+++ hackerrank/AngryProfessor.java
@@ -10,6 +10,10 @@ public class AngryProfessor {
 
     private static final String SPACE = " ";
 
+    enum Answer {
+        YES, NO
+    }
+
     private class Case {
 
         private final int totalStudents;
@@ -20,30 +24,42 @@ public class AngryProfessor {
             this.totalStudents = totalStudents;
             this.minStudentsOnTime = minStudentsOnTime;
             this.arrivals = arrivals;
-
-            validateCase();
         }
 
-        private void validateCase() {
-            if (totalStudents != arrivals.length) {
-                String errorMessage = String.format("Total students expected to be %s, but is %s",
-                    totalStudents, arrivals.length);
-                throw new AssertionError(errorMessage);
-            }
+        public int getTotalStudents() {
+            return totalStudents;
         }
 
-        private int getStudentsOnTime() {
-            int studentsOnTime = 0;
-            for (int arrival : arrivals) {
-                studentsOnTime += (arrival <= 0) ? 1 : 0;
-            }
-            return studentsOnTime;
+        public int getMinStudentsOnTime() {
+            return minStudentsOnTime;
         }
 
-        String classWillBeCancelled() {
-            int studentsOnTime = getStudentsOnTime();
-            return (studentsOnTime >= minStudentsOnTime) ? "NO" : "YES";
+        public int[] getArrivals() {
+            return arrivals;
         }
+    }
+
+    private void validate(Case aCase) {
+        int totalStudents = aCase.getTotalStudents();
+        int[] arrivals = aCase.getArrivals();
+        if (totalStudents != arrivals.length) {
+            String errorMessage = String.format("Total students expected to be %s, but is %s",
+                totalStudents, arrivals.length);
+            throw new AssertionError(errorMessage);
+        }
+    }
+
+    private int countEarly(Case aCase) {
+        int studentsOnTime = 0;
+        for (int arrival : aCase.getArrivals()) {
+            studentsOnTime += (arrival <= 0) ? 1 : 0;
+        }
+        return studentsOnTime;
+    }
+
+    private Answer isClassCancelled(Case aCase) {
+        int studentsOnTime = countEarly(aCase);
+        return (studentsOnTime >= aCase.getMinStudentsOnTime()) ? Answer.NO : Answer.YES;
     }
 
     private Case readCase(BufferedReader reader) throws IOException {
@@ -55,23 +71,24 @@ public class AngryProfessor {
         return new Case(totalStudents, minStudentsOnTime, arrivals);
     }
 
-    private List<Case> readCases() throws IOException {
-        List<Case> cases = new ArrayList<>();
+    private List<Answer> solve() throws IOException {
+        List<Answer> answers = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             int totalCases = Integer.parseInt(reader.readLine());
             for (int i = 0; i < totalCases; i++) {
-                Case inputCase = readCase(reader);
-                cases.add(inputCase);
+                Case aCase = readCase(reader);
+                validate(aCase);
+                answers.add(isClassCancelled(aCase));
             }
         }
-        return cases;
+        return answers;
     }
 
     public static void main(String[] args) throws IOException {
         AngryProfessor angryProfessor = new AngryProfessor();
-        List<Case> cases = angryProfessor.readCases();
-        for (Case inputCase : cases) {
-            System.out.println(inputCase.classWillBeCancelled());
+        List<Answer> answers = angryProfessor.solve();
+        for (Answer answer : answers) {
+            System.out.println(answer);
         }
     }
 }
